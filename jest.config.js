ae8d663be5a450649705ad04dd8d7593265dd892
basarat <basaratali@gmail.com>
2021-09-22 09:03:00 +1000

    uplift

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  "testEnvironment": "node",
-  "roots": [
-    "<rootDir>/src"
-  ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
