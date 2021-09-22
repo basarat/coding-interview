1604ab9c01018d96b913500af5e75d5f091c2e65
basarat <basaratali@gmail.com>
2021-09-23 09:09:27 +1000

    get ready

---
+++ src/getClosingParenthesis/getClosingParenthesis.ts
@@ -0,0 +1,7 @@
+/** 
+ * Given a string, and an opening parenthesis index,
+ *   return the corresponding closing parenthesis index if any
+ */
+export function getClosingParenthesis(input: string, openingIndex: number): number {
+  throw new Error('No closing parenthesis found');
+}
