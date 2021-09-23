d022a259f1bfea947d08f16547acc90ece20283d
basarat <basaratali@gmail.com>
2021-09-23 15:08:12 +1000

    simple

--- src/fizzBuzz/fizzBuzz.test.ts
+++ src/fizzBuzz/fizzBuzz.test.ts
@@ -3,10 +3,10 @@ const restore = global.console.log;
 beforeAll(() => {
   contents = '';
   global.console.log = (x) => contents = contents + (contents ? '\n' : '') + x;
-})
+});
 afterAll(() => {
   global.console.log = restore;
-})
+});
 
 test('fizzBuzz', () => {
   require('./fizzBuzz');
