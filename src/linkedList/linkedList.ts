cf03b80886f99e84c0f3aa706d0f3ea18f40396d
basarat <basaratali@gmail.com>
2021-09-28 08:09:04 +1000

    general idea

---
+++ src/linkedList/linkedList.ts
@@ -0,0 +1,56 @@
+/**
+ * Linked list node
+ */
+export type LinkedListNode<T> = {
+  value: T;
+  next: LinkedListNode<T> | null;
+}
+
+/**
+ * Linked list for items of type T
+ */
+export class LinkedList<T> {
+  public head: LinkedListNode<T> | null = null;
+  public tail: LinkedListNode<T> | null = null;
+
+  /**
+   * Adds an item in O(1)
+   **/
+  add(value: T) {
+    const node = {
+      value,
+      next: null
+    }
+    if (!this.head) {
+      this.head = node;
+    }
+    if (this.tail) {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
+
+  /**
+   * FIFO removal in O(1)
+   */
+  dequeue(): T | null {
+    if (!this.head) return null;
+    const value = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return value;
+  }
+
+  /**
+   * Returns an iterator over the values
+   */
+  *values() {
+    let current = this.head;
+    while (current) {
+      yield current.value;
+      current = current.next;
+    }
+  }
+}
