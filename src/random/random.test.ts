2ef8026449b9e61453e5954286ecec25fa0cdded
basarat <basaratali@gmail.com>
2021-12-14 14:37:49 +1100

    plan

---
+++ src/random/random.test.ts
@@ -0,0 +1,17 @@
+import { randomInt } from './random';
+
+test('Should not include ceiling', () => {
+  const res = [];
+  for (let index = 0; index < 100; index++) {
+    res.push(randomInt(0, 5));
+  }
+  expect(res.some(x => x === 5)).toBeFalsy();
+});
+
+test('Should include one before ceiling', () => {
+  const res = [];
+  for (let index = 0; index < 100; index++) {
+    res.push(randomInt(0, 5));
+  }
+  expect(res.some(x => x === 4)).toBeTruthy();
+});
---
+++ src/random/random.ts
@@ -0,0 +1,3 @@
+for (let index = 0; index < 5; index++) {
+  console.log(1 + Math.floor(Math.random() * 2));
+}
