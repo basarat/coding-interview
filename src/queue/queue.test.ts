2e1e1476561044ae92103e1212e17af5d79600d4
basarat <basaratali@gmail.com>
2022-01-10 12:36:25 +1100

    fix tests

--- src/queue/queue.test.ts
+++ src/queue/queue.test.ts
@@ -2,12 +2,12 @@ import { Queue } from './queue';
 
 test('Basic Tests', () => {
   const queue = new Queue();
-  expect(queue.size()).toBe(0);
+  expect(queue.size()).toBe(0n);
   queue.enqueue('test');
-  expect(queue.size()).toBe(1);
+  expect(queue.size()).toBe(1n);
   queue.enqueue('second');
   expect(queue.dequeue()).toBe('test');
-  expect(queue.size()).toBe(1);
+  expect(queue.size()).toBe(1n);
   expect(queue.dequeue()).toBe('second');
-  expect(queue.size()).toBe(0);
+  expect(queue.size()).toBe(0n);
 });
