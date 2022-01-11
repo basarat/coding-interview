ecbf5759cc064268bbad4e4844bb004a266b0706
basarat <basaratali@gmail.com>
2022-01-12 08:06:10 +1100

    wrap

--- src/removeDuplicates/removeDuplicates.ts
+++ src/removeDuplicates/removeDuplicates.ts
@@ -1,5 +1,5 @@
 /** 
- * Remove all duplicates of items from an array 
+ * Remove all duplicate items from an array 
  */
 export function removeDuplicates<T>(items: T[]): T[] {
   return [...new Set<T>(items)];
